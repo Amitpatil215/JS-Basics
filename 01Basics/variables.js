9237c90a252a562668330182a8c26a82ec9deee8
Amit Patil <amitgpatil215@gmail.com>
2020-10-30 16:15:10 +0530

    read me added

---
+++ 01Basics/variables.js
@@ -0,0 +1,14 @@
+// Not necessory to end line by ;
+
+let name = 'Amit'
+
+let score = 100.8
+let bonus = 20
+const b = 2
+let total = score + bonus
+
+console.log(total)
+
+let firstName = "Amit"
+let lastName = "Patil"
+console.log(firstName + ' ' + lastName);

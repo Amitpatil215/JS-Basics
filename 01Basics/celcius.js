61445fdc3ce039fecf824d28293d9a21d5d3a256
Amit Patil <amitgpatil215@gmail.com>
2020-10-30 19:58:09 +0530

    Fundamental Done

---
+++ 01Basics/celcius.js
@@ -0,0 +1,17 @@
+var score =100
+
+score=130
+
+const bonus=30
+
+var finalScore=score+bonus;
+
+console.log(finalScore)
+
+/// Convert Fharenite to celcius
+
+let tempInFahrenheit=98.6
+
+let celcius = (tempInFahrenheit-32)*5/9
+
+console.log("Temp in Celcius "+ celcius)
---
+++ 01Basics/null&bool.js
@@ -0,0 +1,18 @@
+///Prints undefined
+ var temperature
+
+///Prints null
+// var temperature=null
+
+console.log("Current temperature "+ temperature)
+
+
+//-----Grade System------//
+
+let grade=true
+
+let actualMark=7
+
+grade=(actualMark<10)
+
+console.log(grade)

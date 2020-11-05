18f268154b3e8f1d096523b45c7d6cf8040cfe3c
Amit Patil <amitgpatil215@gmail.com>
2020-11-05 11:06:51 +0530

    predifined methods in js

---
+++ Fundamentals/9_marvels.js
@@ -0,0 +1,47 @@
+// datatype - Array
+
+// const superHeroes = ['Ironman', 'Spiderman', 'capt. America']
+
+// console.log(superHeroes[superHeroes.length - 1]);
+
+// console.log(`We have ${superHeroes.length} super heroes`);
+
+
+//-------------------//
+
+const numbers = ['one', 'two', 'three', 'four', 'Five']
+
+//numbers[1]='something';
+//console.log(numbers);
+
+
+///Removes the first element from an array and returns it
+
+//console.log(numbers.shift());
+//console.log(numbers);
+
+
+///Inserts new elements at the start of an array
+
+//console.log(numbers.unshift('unshift'));
+//console.log(numbers);
+
+
+/// pops out last element in the arry and returns
+
+// console.log(numbers.pop());
+// console.log(numbers);
+
+
+/// Appends new elements to an array ath the end,
+/// and returns the new length of the array.
+
+// console.log(numbers.push('wow'));
+// console.log(numbers);
+
+
+/// Delete from index 3 upto 2 index (i.e 3,4) and replace with
+/// deleteReplaced, ok
+
+// numbers.splice(3, 2,'deleteReplaced','ok')
+// console.log(numbers);
--- Fundamentals/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-if (true) {
-//     console.log("Im second part " + king);
-// }

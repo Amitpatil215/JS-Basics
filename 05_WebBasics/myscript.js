8c6fa4d6fda05da4741b3b27ba2714234bf4da58
Amit Patil <amitgpatil215@gmail.com>
2020-11-08 23:06:14 +0530

    Adding event listener

--- 05_WebBasics/myscript.js
+++ 05_WebBasics/myscript.js
@@ -1,4 +1,24 @@
- console.log(document.getElementById('#idone'));
+// console.log(document.getElementById('#idone'));
 
 // const myElement = document.querySelector('.classone')
 // console.log(myElement);
+
+// const myPElement=document.querySelector('p')
+// myPElement.textContent='I am being changed using JS'
+
+const myPElement = document.querySelectorAll('p')
+
+myPElement.forEach((p) =>
+    p.textContent = 'I am changed using JS loop'
+)
+
+///Creating an elemnet in HTML
+const myNewPara = document.createElement('p')
+myNewPara.textContent = 'I was added via JS'
+document.querySelector('body').appendChild(myNewPara)
+
+///Event listeners
+document.querySelector('button').addEventListener('click', (event) => {
+    event.target.textContent = 'I was clicked'
+
+})
